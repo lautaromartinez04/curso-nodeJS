02bde0da94dd10a591d5f9626ed6a558d002d80f
lautaromartinez04 <lautarom403@gmail.com>
2024-02-28 11:04:46 -0300

    Clase 4

---
+++ Clase 4 - api_libros/data.js
@@ -0,0 +1,6 @@
+const libros = [
+    { id: '1', titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes' },
+    { id: '2', titulo: 'Habitos Atomicos', autor: 'James Clear' },
+    { id: '3', titulo: 'Rayuela', autor: 'Julio Cortazar' }
+    ];
+module.exports = libros
